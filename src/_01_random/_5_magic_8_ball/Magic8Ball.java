c68fdfa3a534f7f28e92ae7d5ac3bbe664c78232
root <root@0caa521c38df>
2021-07-31 00:00:31 +0000

    Magic8ball done 6 lottery numbers in progres.

--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -20,22 +20,32 @@ public class Magic8Ball {
 	//     Limit the random numbers to be between 0 and 3
 	Random ran=new Random();
 	int number = 0;
-	
+	for (int i=0; i<1; i++) {
+		number = ran.nextInt(3+0);
+//		System.out.println(number);
+	}
 	// 4. If the random number is 0
 	// -- tell the user "Yes"
-
+	if(number==0) {
+		System.out.println("yes");
+	}
 	
 	// 5. If the random number is 1
 	// -- tell the user "No"
-
+	if(number==1) {
+		System.out.println("no");
+	}
 	
 	// 6. If the random number is 2
 	// -- tell the user "Maybe you should ask Google?"
-
+	if(number==2) {
+		System.out.println("Maybe you should ask Google");
+	}
 	
 	// 7. If the random number is 3
 	// -- write your own answer
-
+	if(number==3)
+		System.out.println("write your own answer");
 }
 		
 }
---
+++ src/_01_random/_6_lottery_numbers/randomnumbers.java
@@ -0,0 +1,18 @@
+package _01_random._6_lottery_numbers;
+
+import java.util.Random;
+
+public class randomnumbers {
+	public static void main(String[] args) {
+		
+	Random ran=new Random();
+	int lottoterynumber = 0;
+	for (int i=0; i<6; i++) {
+		lottoterynumber=ran.nextInt(10)+1;
+		JOptionPane.pop
+	}
+	
+
+	}	
+
+}
