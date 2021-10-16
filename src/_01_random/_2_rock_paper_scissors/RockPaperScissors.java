205ad331fa83ca74655f5bd1b4166c1909914368
student <student@ip-172-31-61-191.us-west-2.compute.internal>
2021-10-15 18:26:26 -0700

    Spot the error done.

--- src/_01_random/_2_rock_paper_scissors/RockPaperScissors.java
+++ src/_01_random/_2_rock_paper_scissors/RockPaperScissors.java
@@ -75,7 +75,7 @@ public class RockPaperScissors extends JPanel implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         
-        //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing?
+        //1. Run the program and play mvany rounds of Rock Paper Scissors. Does the computer always choose the same thing?
     	 int opponentSelection = 0;
     	 
         //2. Change the value of opponentSelection to be a random number between 0 and 2;
